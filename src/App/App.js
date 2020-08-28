2138d201ba7f97f94584fd8bf4dc03ad4f2350f4
drewrobert <drew.robert26@me.com>
2020-08-27 19:30:27 -0500

    add basic navigation and routing

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import logo from '../logo.svg';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Navigator } from "./Navigator/Navigator";
+import { Home } from "./Home/Home";
 import './App.css';
 
 function App() {
   return (
     <div className="App" data-testid={"App"}>
-
+        <BrowserRouter>
+            <Navigator />
+            <Switch>
+                <Route path={"/"}>
+                    <Home />
+                </Route>
+            </Switch>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/App/Navigator/Navigator.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import RedcycleIcon from "./redcycle-icon.png";
+import "./Navigator.css";
+
+const Navigator = () => {
+    return (
+        <div className={"Navigator"} data-testid={"Navigator"}>
+            <nav className={"navbar"}>
+                <div className={"navbar-brand h1 mb-0"}>
+                    <Link to={"/"}>
+                        <img src={RedcycleIcon} alt={"Redcycle logo"} className={"d-inline-block align-top"} />
+                        {" Redcycle"}
+                    </Link>
+                </div>
+                <ul className={"navbar-nav mr-auto"}>
+                    <li className={"nav-item active"}>
+                        <Link to={"/"} className={"nav-link"}>
+                            Home
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+};
+
+export { Navigator };
