70819305252f1699fdf169278622d88b17d0c556
drewrobert <drew.robert26@me.com>
2020-08-30 14:02:27 -0500

    add github page

---
+++ github-page/precache-manifest.18254903885e043650f2b7476000f7eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9c6495e0811a343513b7e965d653dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f84c4f13f65c5a511aa7",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "c18d2a2a79047327661b",
+    "url": "/static/css/main.35f56485.chunk.css"
+  },
+  {
+    "revision": "f84c4f13f65c5a511aa7",
+    "url": "/static/js/2.825f2469.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.825f2469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18d2a2a79047327661b",
+    "url": "/static/js/main.ea6e4da7.chunk.js"
+  },
+  {
+    "revision": "f0f5fc5275d1ca9c751b",
+    "url": "/static/js/runtime-main.b0b166a4.js"
+  }
+]);
