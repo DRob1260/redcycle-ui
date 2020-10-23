cf22b39d64a00e73a24e13136cdcc9edabd8f2c3
Joey <jmschm1@ilstu.edu>
2020-10-23 14:49:25 -0500

    Added Login with email to UI from the Post Creator page

--- src/components/App/PostCreator/PostCreator.jsx
+++ src/components/App/PostCreator/PostCreator.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './PostCreator.scss'
 import { useLocation } from 'react-router-dom'
+import { Form } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
 const useQuery = () => {
   return new URLSearchParams(useLocation().search)
 }
 
 export const PostCreator = () => {
+  const [formEmail, setFormEmail] = useState()
   const urlQueryParams = useQuery()
-  const username = urlQueryParams.get('username')
+  const email = urlQueryParams.get('email')
+  const onSubmit = (email) => {
+    console.log(email)
+    window.location.href = '?email=' + email;
+  }
 
   return (
     <div>
-      {username ?
-          <div>
-            <h1 className={'PostCreator'}>Welcome, {username}!</h1>
-          </div>
+      {email ?
+        <div>
+          <h1 className={'PostCreator'}>Welcome, {email}!</h1>
+        </div>
         :
-          <div>
-            <h1>Login</h1>
-          </div>
+        <div>
+          <h1>Login</h1>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" onChange={(event)=>setFormEmail(event.target.value)}/>
+            </Form.Group>
+            <Button variant="primary" onClick={()=>onSubmit(formEmail)}>
+              Login
+            </Button>
+          </Form>
+        </div>
       }
     </div>
   )
